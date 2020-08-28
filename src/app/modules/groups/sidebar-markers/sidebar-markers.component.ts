42e661dade781820eae546ea01ac35f222b5502d
Olivia Madrid <oliviajmadrid@gmail.com>
2020-08-28 15:52:01 -0600

    tooltips consistent background colors

--- src/app/modules/groups/sidebar-markers/sidebar-markers.component.ts
+++ src/app/modules/groups/sidebar-markers/sidebar-markers.component.ts
@@ -42,7 +42,6 @@ export class GroupsSidebarMarkersComponent
   groups = [];
   offset = 0;
   moreData: boolean = true;
-  tooltipsAnchor: string = 'right';
   readonly hasNewNavigation: boolean;
   readonly cdnUrl: string;
 
@@ -181,8 +180,6 @@ export class GroupsSidebarMarkersComponent
   }
 
   @HostListener('window:resize') onResize() {
-    this.tooltipsAnchor = window.innerWidth <= 992 ? 'top' : 'right';
-
     if (window.innerWidth > 1000) {
       this.layoutMode = 'desktop';
     } else if (window.innerWidth > 480 && window.innerWidth <= 1000) {
