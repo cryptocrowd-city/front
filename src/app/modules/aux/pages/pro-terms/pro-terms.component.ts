ae007bc11e6a1e6b4f0e7f51d2508b5ea85988a4
Olivia Madrid <oliviajmadrid@gmail.com>
2020-09-21 09:27:38 -0600

    pro terms aux page structure

---
+++ src/app/modules/aux/pages/pro-terms/pro-terms.component.ts
@@ -0,0 +1,7 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'm-aux__proTerms',
+  templateUrl: './pro-terms.component.md',
+})
+export class AuxProTermsComponent {}
