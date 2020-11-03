a51293294c6c59603763e1f9e5b5d41cecaf3bca
Olivia Madrid <oliviajmadrid@gmail.com>
2020-11-03 10:52:06 -0700

    pass tests

--- src/app/common/components/sidebar-widget/sidebar-widget.component.spec.ts
+++ src/app/common/components/sidebar-widget/sidebar-widget.component.spec.ts
@@ -3,7 +3,6 @@ import { Component, Input } from '@angular/core';
 import { RouterTestingModule } from '@angular/router/testing';
 import { Session } from '../../../services/session';
 import { sessionMock } from '../../../../tests/session-mock.spec';
-import sidebarMenu from '../../../modules/analytics/v2/sidebar-menu.default';
 import { ConfigsService } from '../../services/configs.service';
 import { MockService } from '../../../utils/mock';
 import { SidebarWidgetComponent } from './sidebar-widget.component';
