65a310947128f2b692eee3e0e6b38d014ba5768b
Olivia Madrid <oliviajmadrid@gmail.com>
2021-02-23 13:19:25 -0700

    new channel cards

--- src/app/common/components/channel-card/channel-card.component.ts
+++ src/app/common/components/channel-card/channel-card.component.ts
@@ -1,23 +1,74 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  HostListener,
+  Input,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Session } from '../../../services/session';
+import { ConfigsService } from '../../services/configs.service';
+import { UserAvatarService } from '../../services/user-avatar.service';
 
 @Component({
   selector: 'm-channelCard',
   templateUrl: './channel-card.component.html',
   styleUrls: ['./channel-card.component.ng.scss'],
 })
-export class ChannelCardComponent implements OnInit {
+export class ChannelCardComponent implements AfterViewInit {
   @Input() channel: any;
-  // @Input() containerEl: any; ojm todo
+
   @Input() showDescription: boolean = true;
   @Input() showTags: boolean = true;
   @Input() showSubs: boolean = true;
   @Input() showSubscribeButton: boolean = true;
 
-  constructor() {}
+  // display a blue border
+  @Input() featured: boolean = false;
 
-  ngOnInit(): void {}
+  readonly cdnUrl: string;
+  btnIconOnly: boolean = false;
+  size: 'small' | 'medium' | 'large' = 'large';
 
-  //ojm listen(): void {
-  // apply size class based on container width
-  //  }
+  constructor(
+    protected userAvatar: UserAvatarService,
+    protected session: Session,
+    configs: ConfigsService
+  ) {
+    this.cdnUrl = configs.get('cdn_url');
+  }
+
+  ngAfterViewInit(): void {
+    this.onResize();
+  }
+
+  @ViewChild('channelCard') channelCardEl: ElementRef;
+
+  @HostListener('window:resize')
+  onResize() {
+    const channelCardWidth = this.channelCardEl.nativeElement.offsetWidth;
+
+    if (channelCardWidth <= 350) {
+      if (channelCardWidth > 250) {
+        this.size = 'medium';
+      } else {
+        this.size = 'small';
+      }
+    } else {
+      this.size = 'large';
+    }
+
+    this.btnIconOnly = this.size === 'medium';
+  }
+
+  public getAvatarSrc(): Observable<string> {
+    if (this.channel.guid === this.session.getLoggedInUser().guid) {
+      return this.userAvatar.src$;
+    }
+    return of(
+      `${this.cdnUrl}icon/${this.channel.guid}/medium/${this.channel.icontime}`
+    );
+  }
 }
